090aa42ec50acbef9730f5366587d9dbe2b0b3f6
HenryLudenaPyX <ludenahenry2@gmail.com>
2025-05-18 00:39:31 -0500

    It is a change in the calculator execute class

--- src/main/java/org/example/Calculator/CalculatorExecute.java
+++ src/main/java/org/example/Calculator/CalculatorExecute.java
@@ -9,5 +9,7 @@ public class CalculatorExecute {
         System.out.println("c.addition(4,7) = " + addition);
         int subtraction = c.subtract(7, 1);
         System.out.println("c.subtraction(7,1) = " + subtraction);
+
+        System.out.println(" == It is a change ==");
     }
 }
